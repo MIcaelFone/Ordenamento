2181c850404a7fbc98913ad3d0658729dabbc22c
Micael Daniel Pardim Fone <104154840+MicaelDanielPardim@users.noreply.github.com>
2023-11-12 19:33:15 -0300

    Arrumando comentários do insectation Sort

--- InsectationSort.java
+++ InsectationSort.java
@@ -6,10 +6,10 @@ public class InsectationSort {
 
     public void ordenarInsectationSort(int [] lista){
         
-        for (int i =1; i < lista.length; i++){ // posição do elemento a ser inserido
+        for (int i =1; i < lista.length; i++){
             
             int j = i; // posição do elemento a ser inserido
-            while(j > 0 && lista[j]< lista[j-1]){ // enquanto o elemento a ser inserido for menor que o elemento anterior
+            while(j > 0 && lista[j]< lista[j-1]){ 
                 int elemento_temporario = lista[j];
                 System.out.println( "Troca de posição:" + elemento_temporario + " da posição "+j+" com o "+lista[j-1]+" da posição "+(j-1)+".");
                 System.out.println("Lista antes da troca:"+Arrays.toString(lista));

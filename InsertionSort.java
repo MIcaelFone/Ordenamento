2207fc6ffa1e74f4359e0a7eb29b61f5c1d17134
Micael Daniel Pardim Fone <104154840+MicaelDanielPardim@users.noreply.github.com>
2023-11-13 17:19:01 -0300

    Arrumando  erros da main e e insertionSort

--- InsertionSort.java
+++ InsertionSort.java
@@ -8,7 +8,7 @@ public class InsertionSort {
         for (int i =1; i < lista.length; i++){
             
             int j = i; // posição do elemento a ser inserido
-            while(j > 0 && lista[j]< lista[j-1]){ // enquanto o elemento a ser inserido for menor que o elemento anterior e  
+            while(j > 0 && lista[j]< lista[j-1]){ // enquanto o elemento a ser inserido for menor que o elemento anterior e não for o primeiro elemento da lista
                 int elementoTemporario = lista[j];
                 int elementoAnterior = lista[j-1];
                 System.out.println( "Troca de posição:" +elementoTemporario + " da posição "+j+" com o "+elementoAnterior+" da posição "+(j-1)+".");
